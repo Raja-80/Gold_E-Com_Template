8e27483ae120553002f1a04c4622471f59e3b991
badr aoulad agrir <50755123+badrAG@users.noreply.github.com>
2024-04-09 02:04:38 +0000

    DEV :: add track event

--- pages/thanks/index.vue
+++ pages/thanks/index.vue
@@ -56,6 +56,17 @@ export default {
         item_ids: pixelData.content_ids,
       });
     }
+    if (this.$store.state.settings.analitycs_shere &&
+        this.$store.state.settings.analitycs_shere[0] &&
+        this.$store.state.settings.analitycs_shere[0]['data-website-id'] && window.umami) {
+        window.umami.track('Purchase',{
+            price: pixelData.total,
+            orderId: this.$route.query.code,
+            currency: this.$store.state.currency && this.$store.state.currency.code ? this.$store.state.currency.code : "",
+            item_ids: pixelData.content_ids,
+            click_id: this.$route.query.ScCid ? this.$route.query.ScCid : null
+          })
+    }
     // take OrderId from url
     const url = new URL(window.location.href);
     const params = new URLSearchParams(url.search);
