3cf07471dce421761bcef1249bd927034f7dfe43
Abdelali-ErRafik <84728514+Abdelali-ErRafik@users.noreply.github.com>
2024-05-06 16:45:08 +0100

    DEV :: Fix variants

--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -207,7 +207,7 @@
                             <p class="pt-3 text-sml">{{ item.description }}</p>
                             <!-- short description -->
                             <!-- variant -->
-                            <div v-show="!settings.checkout_required_fields.show_variant_on_checkout">
+                            <div v-show="!$settings.checkout_required_fields.show_variant_on_checkout">
                                 <si-product-variants class="flex text-sml" v-if="item.type=='variable'" :options="item.options" :images="item.images" :variants="item.variants" @selected="variantSelected"></si-product-variants>
                             </div>
                             <!-- variant -->
